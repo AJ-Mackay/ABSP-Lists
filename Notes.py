efbc31e6fd03fca3007ba1889fc78250033d5ed3
Ruiner85 <paul.m.mackay85@gmail.com>
2020-08-31 14:16:09 +0100

    Add notes on first three lessons

---
+++ Notes.py
@@ -0,0 +1,6 @@
+# Module 6 - Lists:
+
+# List Data Type = All typed in the Shell
+# For Loops with Lists, Multiple Assignment & Augmented Operators = All typed in the Shell
+# List Methods = All typed in the Shell
+
